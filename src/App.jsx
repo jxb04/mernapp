ef586b615d18184b913e128c206a6f56f148de2a
Joel Bernasky <joel_bernasky@hotmail.com>
2019-02-25 09:25:56 -0500

    event handling

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,8 @@ class IssueList extends React.Component {
         super();
         this.state = { issues: [] };
 
-        setTimeout(this.createTestIssue.bind(this), 2000);
+        this.createTestIssue = this.createTestIssue.bind(this);
+        setTimeout(this.createTestIssue, 2000);
     }
 
     componentDidMount() {
@@ -106,6 +107,7 @@ class IssueList extends React.Component {
               <IssueFilter />
               <hr />
               <IssueTable issues={this.state.issues} />
+              <button onClick={this.createTestIssue}>Add</button>
               <hr />
               <IssueAdd />
             </div>
