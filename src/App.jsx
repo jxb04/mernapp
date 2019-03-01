8688854fbd996a2d863ec9b22ec67392ea0139f1
Joel Bernasky <joel_bernasky@hotmail.com>
2019-02-28 21:36:40 -0500

    update client error handling

--- src/App.jsx
+++ src/App.jsx
@@ -101,21 +101,31 @@ class IssueList extends React.Component {
     }
 
     createIssue(newIssue) {
-      fetch('/api/issues', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newIssue),
-      }).then(response => response.json()
-        ).then(updatedIssue => {
-          updatedIssue.created = new Date(updatedIssue.created);
-          if (updatedIssue.completionDate)
-          updatedIssue.completionDate = new Date(updatedIssue.completionDate);
-          const newIssues = this.state.issues.concat(updatedIssue);
-          this.setState({ issues: newIssues });
+        fetch('/api/issues', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newIssue),
+        }).then(response => {
+            if (response.ok) {
+                response.json().then(updatedIssue => {
+                    updatedIssue.created = new Date(updatedIssue.created);
+                    if (updatedIssue.completionDate) updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+                    const newIssues = this.state.issues.concat(updatedIssue);
+                    this.setState({
+                        issues: newIssues
+                    });
+                });
+            } else {
+                response.json().then(error => {
+                    alert("Failed to add issue: " + error.message)
+                });
+            }
         }).catch(err => {
-          alert("Error in sending data to server: " + err.message);
+            alert("Error in sending data to server: " + err.message);
         });
-      }
+    }
 
     render() {
         return (
